b217932b631551e5113c99bbc5382abb96e0e449
superman <1193315416@qq.com>
2017-11-20 18:46:46 +0800

    add

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var list = [
+
+/*设置本地存储*/
+var store = {
+        save(key,value){
+		localStorage.setItem(key,JSON.stringify(value));
+},
+        fetch(key){
+            return JSON.parse(localStorage.getItem(key)) || [];
+        }
+}
+
+/*var list = [
     {
         title: "吃饭打豆豆",
         isChecked:false,
@@ -8,21 +19,52 @@ var list = [
       isChecked:true
     }
 ]
+*/
+var list = store.fetch("label");
 
-new Vue({
+var filter={
+    all:function(list){
+        return list;
+    },
+    finished:function(list){
+        return list.filter(function(item){
+            return item.isChecked;
+        })
+    },
+    unfinished:function(list){
+        return list.filter(function(item){
+            return !item.isChecked;
+        })
+    }
+}
+
+var vm=new Vue({
     el:".main",
     data: {
         list: list,
         todo:"",
         edtorTodos:"",
         beforeTitle:"",
+        visibility:"all"
+    },
+    watch:{
+        list:{
+            handler:function(){   ////监控list这个属性，当这个属性对应的值发生变化就会执行函数
+                store.save("label",this.list);
+            },
+            deep:true    //对象内部的属性监听，也叫深度监听  
+        }
     },
     computed:{    //返回计算有多少条任务
         noCheckLength:function(){
             return this.list.filter(function(item){
                 return !item.isChecked
             }).length
+        },
+        filterList:function(){
+            return filter[this.visibility]?filter[this.visibility](list):list
         }
+
     },
     methods: {
         addTodo(){
@@ -51,7 +93,6 @@ new Vue({
    directives:{    /*自定义事件*/
         "focus":{
             update(el,binding){
-                console.log(binding)
                 if(binding.value){
                     el.focus();
                 }
@@ -61,3 +102,10 @@ new Vue({
       }
 
 })
+
+function watchHashChange(){
+     var hash=window.location.hash.slice(1)
+     vm.visibility=hash
+}
+watchHashChange()
+window.addEventListener("hashchange",watchHashChange)
